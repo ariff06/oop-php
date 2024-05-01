2c4bf7853322b7030935168dd3545d9129652aa7
ariff06 <ariffauzy74@gmail.com>
2024-05-01 11:26:21 +0700

    Add OOP Animal in php

---
+++ ape.php
@@ -0,0 +1,15 @@
+<?php
+
+class ape extends animal {
+    public function __construct() {
+        $this->legs = 2;
+    }
+
+    public $yell;
+
+    public function yell() {
+        return $this -> yell = "Auooo";
+    }
+}
+
+?>
